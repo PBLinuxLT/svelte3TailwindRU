5d5be8527b8625c5f8784cb17e520a74f610d1d2
Paul Brown <pbLinuxLT@gmail.com>
2020-05-10 15:28:20 +0100

    hello tailwind css

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
   </div>
   <div class="mt-4 md:mt-0 md:ml-6">
     <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
-    <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
+    <a href="/" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
     <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
   </div>
 </div>
